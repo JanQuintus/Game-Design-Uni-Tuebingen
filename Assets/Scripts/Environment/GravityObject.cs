b585dba485047d4c0fbb2aa2e93df5dcf68b3307
Marvyn <marvin.doebel@student.uni-tuebingen.de>
2020-11-29 18:10:36 +0100

    Added gravityObject script
    -switched off global gravity
    -gravityObjects automatically add rigid body component to game object
    -only gravity Objects have gravity
    -added some debug materials and a debug scene

---
+++ Assets/Scripts/Environment/GravityObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityObject : MonoBehaviour
+{
+    [SerializeField] private Vector3 localGravity = new Vector3(0, -9.81f, 0); //initialization with standard gravity
+    private Rigidbody rb;
+    [SerializeField] private bool useGravity = true;
+
+    private int counter = 0;
+
+    void Awake()
+    {
+        gameObject.AddComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(rb.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter = counter + 1;
+        if (counter == 100)
+        {
+            SetLocalGravity(new Vector3(-9, 0, 0));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (useGravity)
+        {
+            rb.AddForce(localGravity, ForceMode.Force); // continuous force with using mass of object
+        }
+    }
+
+    public void SetLocalGravity(Vector3 newGravityVector)
+    {
+        localGravity = newGravityVector;
+    }
+
+    public void SetUseGravity(bool isGravity)
+    {
+        useGravity = isGravity;
+    }
+}
