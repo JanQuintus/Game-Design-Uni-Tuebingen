3a4123155ec2c19fa609185b286317bb1d5c15d9
Marvyn <marvin.doebel@student.uni-tuebingen.de>
2020-12-12 20:03:08 +0100

    Added Tractor Beam

---
+++ Assets/Scripts/Tools/TheBeam.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheBeam : MonoBehaviour
+{
+
+    public float pullForce = 4;
+    public float minDist = 5f;
+
+    private Collider objectInBeam;
+    private float dist;
+
+    private bool isOccupied = false;
+
+    private Vector3 displacement = new Vector3(0,0,0); // needed for displacement ob object inside beam due to scoll
+    private int displacementIntensity = 0;
+
+    private float maxDisplacement = 3; // adjust to beam length
+
+    private float windUpStrength = 0; // this is the number which is decreased to visualize windup (stronger windup = object is closer to player)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //TODO: add offset to move object in beam via scrolling (objectInBeam.TransformDirection(vector);)
+
+        if (isOccupied)
+        {
+            Vector3 localPos = transform.InverseTransformDirection(transform.forward) * displacementIntensity;
+            Vector3 lPos = transform.InverseTransformDirection(transform.forward * -1) * windUpStrength;
+            Vector3 displace = transform.InverseTransformDirection(transform.forward * -1) * 9; // adjust floating to be closer to the player to allow more the beam to have more range
+            Vector3 targetPos = transform.position + transform.TransformDirection(localPos) + transform.TransformDirection(lPos) + transform.TransformDirection(displace);
+
+            Vector3 objectPos = objectInBeam.transform.position;
+
+            float dx = objectPos.x - targetPos.x;
+            float dy = objectPos.y - targetPos.y;
+            float dz = objectPos.z - targetPos.z;
+
+            dist = Mathf.Sqrt(dx * dx + dz * dz + dy * dy);
+
+            Vector3 forceDirection = targetPos - objectPos;
+            Rigidbody rb = objectInBeam.GetComponent<Rigidbody>();
+
+            if (dist > minDist)
+            {
+
+                rb.AddForce(forceDirection.normalized * pullForce, ForceMode.Impulse); // apply force towards center
+
+            }
+            else
+            {
+                rb.velocity = rb.velocity * (1 - (5 * Time.deltaTime));
+                rb.AddForce(forceDirection.normalized * pullForce, ForceMode.Impulse);
+
+                //Vector3 lPos = transform.InverseTransformDirection(transform.forward * -1) * windUpStrength;
+                //rb.AddForce(transform.TransformDirection(lPos), ForceMode.Impulse);
+            }
+        }
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if ((!isOccupied) && (other.GetComponent("GravityObject") != null))
+        {
+            objectInBeam = other.GetComponent<Collider>();
+            isOccupied = true;
+        }
+
+
+    }
+
+    public void turnOffBeam()
+    {
+        isOccupied = false;
+        objectInBeam = null;
+        displacementIntensity = 0;
+        windUpStrength = 0;
+        gameObject.SetActive(false);
+    }
+
+    public void turnOnBeam()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void shoot(float shootForce, Vector3 forceDirection)
+    {
+        if (objectInBeam != null)
+        {
+            Rigidbody rb = objectInBeam.GetComponent<Rigidbody>();
+            rb.AddForce(forceDirection.normalized * shootForce, ForceMode.Impulse);
+        }
+    }
+
+    public void SetDisplacement(Vector3 newDir)
+    {
+        displacement = newDir;
+    }
+
+    public void changeDisplacementIntensity(float delta)
+    {
+        //move object in the distance
+        if (displacementIntensity <= maxDisplacement)
+        {
+            if (delta > 0)
+            {
+                displacementIntensity = displacementIntensity + 1;
+            }
+
+        }
+
+        //move object closer to the player
+        if (displacementIntensity >= -maxDisplacement)
+        {
+            if (delta < 0)
+            {
+                displacementIntensity = displacementIntensity - 1;
+            }
+        }
+    }
+
+    public void setWindup(float value)
+    {
+        windUpStrength = value;
+    }
+}
---
+++ Assets/Scripts/Tools/TractorBeamTool.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TractorBeamTool : ATool
+{
+
+    private TheBeam TheBeam;
+
+    private bool windUp;
+    private float shootForce = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TheBeam = GameObject.Find("TheBeam").GetComponent<TheBeam>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (windUp && (shootForce < 40))
+        {
+            shootForce = shootForce + 0.5f;
+            TheBeam.setWindup(shootForce/20); // needed for pulling of object towards player if winded up
+        }
+    }
+
+    public override void Shoot(Ray ray, bool isRelease = false, bool isRightClick = false)
+    {
+
+        //rmb pressed
+        if (isRightClick && !isRelease)
+        {
+            TheBeam.turnOnBeam();
+        }
+
+        //rmb released
+        if (isRightClick && isRelease)
+        {
+            TheBeam.turnOffBeam();
+        }
+
+        //lmb released
+        if (!isRightClick && isRelease)
+        {
+            //shoot object in beam
+            Vector3 shootDirection = ray.direction;
+            TheBeam.shoot(shootForce, shootDirection);
+            TheBeam.turnOffBeam();
+
+            // reset windup
+            windUp = false;
+            shootForce = 10;
+        }
+
+        //lmb pressed
+        if (!isRightClick && !isRelease)
+        {
+            windUp = true;
+        }
+    }
+
+    public override void Reset(bool isRelease)
+    {
+
+    }
+
+    // delta = -120, 0, 120 depending on scroll direction
+    public override void Scroll(float delta) 
+    {
+        TheBeam.changeDisplacementIntensity(delta);
+    }
+}
