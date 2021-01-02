e3f25077c2362d1f87af07d7cee7910eaec795da
VeysTheGreat <li.fe2117@gmx.net>
2021-01-02 19:23:14 +0100

    added npcStartTalk.cs and scene with NPC Capsule and DialoguePanel on a Canvas

---
+++ Assets/Scripts/NPC/Dialogue/npcStartTalk.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class npcStartTalk : AInteractive
+{
+    // Panel, bool for active dialogue box initialize
+    public RectTransform Panel;
+
+    public override void Interact(bool isRelease) // on Interact
+    {
+        if (!isRelease)
+        {
+            // if Panel isn't active already
+            if (Panel.gameObject.activeSelf == false)
+            {
+                // scale down beforehand so it can get scaled up once visible
+                Panel.DOScaleX(0.1f, 0f);
+                Panel.DOScaleY(0.1f, 0f);
+
+                // open Panel
+                Panel.gameObject.SetActive(true);
+
+                // scale up
+                Panel.DOScaleX(1f, 0.2f);
+                Panel.DOScaleY(1f, 0.2f);
+            }
+
+            // otherwise close it ( scale down and hide )
+            else
+            {
+                Panel.DOScaleX(0.1f, 0.2f);
+                Panel.DOScaleY(0.1f, 0.2f);
+                Invoke("hidePanel", 0.2f);
+            }
+        }
+    }
+
+
+    private void hidePanel() // hidePanel, what does it do? :flushed:
+    {
+        Panel.gameObject.SetActive(false);
+    }
+}
