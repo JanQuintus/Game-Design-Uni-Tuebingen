6770b5de93d6a9c126a81f067b2e1733c70bf4a8
Jan Quintus <jan.quintus@web.de>
2021-01-23 21:00:05 +0100

    problems

---
+++ Assets/Scripts/AI/MutantAnimationEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MutantAnimationEvents : MonoBehaviour
+{
+    public void AttackPlayer()
+    {
+        if(Vector3.Distance(transform.position, PlayerController.Instance.transform.position) <= 3f)
+        {
+            PlayerController.Instance.GetComponentInParent<Health>().Damage(10);
+        }
+    }
+}
---
+++ Assets/Scripts/AI/RagdollController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+    private void Awake()
+    {
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+            rb.isKinematic = true;
+        foreach (Collider col in GetComponentsInChildren<Collider>())
+            col.enabled = false;
+        foreach (GravityObject go in GetComponentsInChildren<GravityObject>())
+            go.enabled = false;
+
+    }
+
+    public void EnableRagdoll(bool gravity = true, float mass = -1)
+    {
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.isKinematic = false;
+            rb.useGravity = gravity;
+            if (mass > 0)
+                rb.mass = (rb.mass / 70f) * mass;
+        }
+        foreach (Collider col in GetComponentsInChildren<Collider>())
+            col.enabled = true;
+        foreach (GravityObject go in GetComponentsInChildren<GravityObject>())
+            go.enabled = true;
+    }
+}
