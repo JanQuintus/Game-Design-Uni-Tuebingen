ddd5fb80be8fc7a3de29de792665c78d8a244059
Marvyn <marvin.doebel@student.uni-tuebingen.de>
2021-02-01 19:04:07 +0100

    added second riddle in dump

---
+++ Assets/Scripts/Environment/Dumpreset_Button.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dumpreset_Button : AInteractive
+{
+    [SerializeField] GameObject resetTrigger;
+
+    public override void Interact(bool isRelease)
+    {
+        resetTrigger.GetComponent<dumpreset>().Release();
+    }
+}
