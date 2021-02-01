ca700002a2e5277f4d7ddf29f0e4d1ca75b9bd70
Tobias Sautter <tobiasm4@gmx.net>
2021-02-01 19:41:59 +0100

    added living room, changed shader for AInteractive

--- Assets/Scripts/AInteractive.cs
+++ Assets/Scripts/AInteractive.cs
@@ -2,20 +2,21 @@
 
 public abstract class AInteractive : MonoBehaviour
 {
-    [SerializeField] protected Material outlineMaterial;
+    [SerializeField] protected Renderer outlineMaterial;
+    private int _shader_active = Shader.PropertyToID("_Outline");
 
     public abstract void Interact(bool isRelease);
 
     public virtual void ShowOutline()
     {
         if(outlineMaterial != null)
-            outlineMaterial.SetFloat("_Outline", 1f);
+            outlineMaterial.material.SetFloat(_shader_active, 1f);
     }
 
     public virtual void HideOutline()
     {
         if(outlineMaterial != null)
-            outlineMaterial.SetFloat("_Outline", 0f);
+            outlineMaterial.material.SetFloat(_shader_active, 0f);
     }
 
     public virtual string GetText() => "";
