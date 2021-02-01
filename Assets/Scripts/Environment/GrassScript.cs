ce248564b2e517a9e434135a5958cbf65b60ee80
niklas-henle <niklas.henle@gmail.com>
2021-02-01 18:54:02 +0100

    Temp. gras and water shader.

---
+++ Assets/Scripts/Environment/GrassScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassScript : MonoBehaviour
+{
+    public Vector3 position;
+    public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("updateMaterial");
+    }
+
+    // Update is called once per frame
+    IEnumerator updateMaterial()
+    {
+        while (true)
+        {
+            position = PlayerController.Instance.transform.position;
+
+            material.SetVector("playerPosition", position);
+            yield return null;
+
+        }
+
+    }
+}
