1e54c1cd442cd4cba5b293a1185e956c299a29b8
Tobias Sautter <tobiasm4@gmx.net>
2021-02-09 17:51:09 +0100

    gymShizzle

--- Assets/Scripts/AInteractive.cs
+++ Assets/Scripts/AInteractive.cs
@@ -2,24 +2,41 @@
 
 public abstract class AInteractive : MonoBehaviour
 {
-    [SerializeField] protected Renderer outlineMaterial;
+    [SerializeField] protected Outline outline;
+    [SerializeField] protected float outlineWidth;
     private int _shader_active = Shader.PropertyToID("_Outline");
+
     [SerializeField] protected AudioClip hoverClip;
 
     public abstract void Interact(bool isRelease);
 
     public virtual void Hover()
     {
-        if(outlineMaterial != null)
-            outlineMaterial.material.SetFloat(_shader_active, 1f);
+        if(outline != null) {
+            //outlineMaterial.material.SetFloat(_shader_active, 1f);
+            //var outline = gameObject.GetComponent<Outline>();
+            outline.OutlineMode = Outline.Mode.OutlineAll;
+            outline.OutlineWidth = outlineWidth;
+            
+        }
+        
+
         if (hoverClip != null)
             SoundController.Instance.PlaySoundAtLocation(hoverClip, transform.position);
     }
 
     public virtual void Unhover()
     {
-        if(outlineMaterial != null)
-            outlineMaterial.material.SetFloat(_shader_active, 0f);
+        if (outline != null)
+        {
+            //outlineMaterial.material.SetFloat(_shader_active, 0f);
+            //var outline = gameObject.GetComponent<Outline>();
+            outline.OutlineWidth = 0f;
+            outline.OutlineMode = Outline.Mode.OutlineHidden;
+            
+
+        }
+
     }
 
     public virtual string GetText() => "";
