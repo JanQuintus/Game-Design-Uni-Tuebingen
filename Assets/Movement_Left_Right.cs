f124dbc33d8926a81a602d0e281a36662be1d9ca
davitBulatsashvili <dave_bulac2@mail.ru>
2021-02-19 19:03:27 +0100

    Romm for Gravity Gun

---
+++ Assets/Movement_Left_Right.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Left_Right : MonoBehaviour
+{
+    private Vector3 LeftWall = new Vector3(4.6f, 4.5f, -0.36f);
+    private Vector3 RightWall = new Vector3(-5.3f, 4.5f, -0.36f);
+    public float speed;
+    private Rigidbody rb;
+    private float rightWall = 38f;
+    private float lefttWall = 32f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(transform.position + (-transform.right * speed * Time.deltaTime));
+        
+        if (transform.position.x <= -38f)
+        {
+            //rb.transform.position = rb.transform.position;
+            rb.MovePosition(transform.position + (transform.right * speed * Time.deltaTime));
+            Debug.Log("Hitted Right Wall");
+        }
+        else if(transform.position.x >= -32f)
+        {
+            rb.transform.position = rb.transform.position;
+            rb.MovePosition(transform.position + (-transform.right * speed * Time.deltaTime));
+            Debug.Log("Hitted Left Wall");
+        }
+        
+        Debug.Log(transform.position.x);
+        
+    }
+}
---
+++ Assets/Movement_Up_Down.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Up_Down : MonoBehaviour
+{
+    private Vector3 Roof = new Vector3(-0.4f, 9.5f, -0.36f);
+    private Vector3 Floor = new Vector3(0.4f, 0, -0.36f);
+    public float speed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.useGravity)
+        {
+            if (rb.transform.position.y >= Roof.y - 10)
+            {
+
+                rb.transform.position = rb.transform.position;
+                //rb.useGravity = false;
+                //Debug.Log(rb.transform.position.y);
+            }
+            else
+            {
+                rb.MovePosition(transform.position + (transform.up * speed * Time.deltaTime));
+                //Debug.Log(rb.transform.position.y);
+            }
+                
+        }
+        else
+        {
+            if (rb.transform.position.y + 7 <= Floor.y + 1)
+            {
+                rb.transform.position = rb.transform.position;
+                //rb.useGravity = true;
+                //Debug.Log(rb.transform.position.y);
+            }
+            else
+            {
+                rb.MovePosition(transform.position + (-transform.up * speed * Time.deltaTime));
+                //Debug.Log(rb.transform.position.y);
+            }
+        }
+    }
+}
