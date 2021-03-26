3a3702893fe4b70665e32af58617461dfd1cc3fd
Marvyn <marvin.doebel@student.uni-tuebingen.de>
2021-03-26 17:13:44 +0100

    some polishing and lifts in hub plus scene connections

---
+++ Assets/Scripts/Environment/VelocityReset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityReset : MonoBehaviour
+{
+
+    private void OnTriggerExit(Collider other)
+    {
+        GravityObject go = Utils.FindGravityObject(other);
+        Rigidbody rb = go.GetRB();
+        rb.velocity = rb.velocity / 2;
+    }
+}
